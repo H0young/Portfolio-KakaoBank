dcb73876b693e7350beb3d95313c7ecea70886e9
Hoyoung Kim <ghdud9606@gmail.com>
2020-07-27 16:14:32 +0900

    Edit footer style

--- html&js/carouselFunc.js
+++ html&js/carouselFunc.js
@@ -13,8 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    var slides = document.getElementsByClassName("intro8-img");
     if (n > slides.length) {
         slideIndex = 1
     }
@@ -24,11 +23,8 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
 }
 
-https: //www.w3schools.com/howto/howto_js_slideshow.asp
+// https: //www.w3schools.com/howto/howto_js_slideshow.asp
